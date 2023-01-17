d78c9d986e7d16ac85c4f98d6862a77e870b200c
Deen <htcuser.wkp@gmail.com>
2023-01-17 06:12:48 +0530

    solution changes

--- Sample.ConAPI/Program.cs
+++ Sample.ConAPI/Program.cs
@@ -18,6 +18,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true));
+
 app.UseAuthorization();
 
 app.MapControllers();
