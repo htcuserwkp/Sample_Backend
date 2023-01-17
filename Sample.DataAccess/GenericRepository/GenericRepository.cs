7ae4efa35612c64cc01a90c8fc8d3d46afd3e84e
Deen <htcuser.wkp@gmail.com>
2023-01-17 16:45:22 +0530

    GenericRepo Implementation, Predicate Builder

--- Sample.DataAccess/GenericRepository/GenericRepository.cs
+++ Sample.DataAccess/GenericRepository/GenericRepository.cs
@@ -1,47 +1,114 @@
 ﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Sample.Common.Helpers.PredicateBuilder;
 using Sample.DataAccess.Entities;
 
 namespace Sample.DataAccess.GenericRepository;
 
 public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
 {
-    public Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>>? predicate = null)
+    private readonly DbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(DbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _dbSet = context.Set<TEntity>();
     }
 
-    public Task<TEntity> GetByIdAsync(long id)
+    public virtual async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>>? predicate = null)
     {
-        throw new NotImplementedException();
+        IQueryable<TEntity> queryable = _dbSet;
+
+        var predicateBuilder = GetPredicateBuilder(predicate);
+
+        queryable = queryable.Where(predicateBuilder);
+
+        return await queryable.ToListAsync();
     }
 
-    public Task<IEnumerable<TEntity>> GetAllAsync()
+    public virtual async Task<TEntity> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        return (await _dbSet.FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted))!;
     }
 
-    public Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>>? predicate = null)
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return await _dbSet.Where(e => !e.IsDeleted).ToListAsync();
     }
 
-    public Task AddAsync(TEntity entity)
+    public virtual async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>>? predicate = null)
     {
-        throw new NotImplementedException();
+        IQueryable<TEntity> queryable = _dbSet;
+
+        var predicateBuilder = GetPredicateBuilder(predicate);
+
+        return (await queryable.FirstOrDefaultAsync(predicateBuilder))!;
     }
 
-    public Task UpdateAsync(TEntity entity)
+    public virtual async Task AddAsync(TEntity entity)
     {
-        throw new NotImplementedException();
+        try
+        {
+            ValidateEntity(entity);
+            await _dbSet.AddAsync(entity);
+        }
+        catch (DbUpdateException e)
+        {
+            throw new Exception("Exception occurred while inserting data", e);
+        }
     }
 
-    public Task DeleteAsync(long id)
+    public virtual async Task UpdateAsync(TEntity entity)
     {
-        throw new NotImplementedException();
+        await Task.Run(() =>
+        {
+            try
+            {
+                ValidateEntity(entity);
+                _dbSet.Attach(entity);
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+            catch (DbUpdateException e)
+            {
+                throw new Exception("Exception occurred while updating data", e);
+            }
+        });
     }
 
-    public Task DeleteAsync(TEntity entity)
+    public virtual async Task DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        var entity = await GetByIdAsync(id);
+        await DeleteAsync(entity);
     }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        ValidateEntity(entity);
+        entity.IsDeleted = true;
+        await UpdateAsync(entity);
+    }
+
+    #region Provate Methods
+    private static void ValidateEntity(TEntity entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentException($"Entity cannot be null");
+        }
+    }
+
+    private static Expression<Func<TEntity, bool>> GetPredicateBuilder(Expression<Func<TEntity, bool>>? predicate)
+    {
+        var predicateBuilder = PredicateBuilder.True<TEntity>();
+        predicateBuilder = predicateBuilder.And(c => !c.IsDeleted);
+
+        if (predicate != null)
+        {
+            predicateBuilder = predicateBuilder.And(predicate);
+        }
+
+        return predicateBuilder;
+    }
+    #endregion
 }
