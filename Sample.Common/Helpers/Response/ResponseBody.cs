8d0db6883678911d4887874d57c044080461aa26
Deen <htcuser.wkp@gmail.com>
2023-01-19 16:38:38 +0530

    Generic ResponseBody

---
+++ Sample.Common/Helpers/Response/ResponseBody.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Sample.Common.Helpers.Response;
+
+public class ResponseBody<TDataType>
+{
+    public ResponseBody()
+    {
+        Status = HttpStatusCode.OK;
+    }
+
+    public HttpStatusCode Status { get; set; }
+    public IEnumerable<string>? Warnings { get; set; }
+    public string? Message { get; set; }
+    public TDataType? Data { get; set; }
+}
