12e391f34d3c6e905798125f1b38fcd38970f25e
Deen <htcuser.wkp@gmail.com>
2023-02-26 15:41:11 +0530

    Remove mapping for test apis

--- Sample.ConAPI/Controllers/SampleControllers/MathApiController.cs
+++ Sample.ConAPI/Controllers/SampleControllers/MathApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Sample.API.Controllers.SampleControllers;
 
-public class MathApiController : BaseApiController
+public class MathApiController : ControllerBase
 {
     private readonly ILogger<MathApiController> _logger;
 
--- Sample.ConAPI/Controllers/SampleControllers/WeatherForecastController.cs
+++ Sample.ConAPI/Controllers/SampleControllers/WeatherForecastController.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Sample.API.Controllers.SampleControllers;
 
-public class WeatherForecastController : BaseApiController
-{
+public class WeatherForecastController : ControllerBase {
+
     private static readonly string[] Summaries = {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
@@ -12,17 +12,14 @@ public class WeatherForecastController : BaseApiController
     private readonly ILogger<WeatherForecastController> _logger;
     private readonly IMemoryCache _cache;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache cache)
-    {
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache cache) {
         _logger = logger;
         _cache = cache;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get([FromQuery] int noOfDays = 5)
-    {
-        return Enumerable.Range(0, noOfDays).Select(index => new WeatherForecast
-        {
+    public IEnumerable<WeatherForecast> Get([FromQuery] int noOfDays = 5) {
+        return Enumerable.Range(0, noOfDays).Select(index => new WeatherForecast {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
@@ -30,18 +27,14 @@ public class WeatherForecastController : BaseApiController
             .ToArray();
     }
 
-    [HttpGet("ForTime")]
-    public IActionResult GetForecastByTime([FromQuery] DateTime startDate, [FromQuery] DateTime endDate)
-    {
-        try
-        {
-            if (!ModelState.IsValid)
-            {
+    [HttpGet(Name = "ForTime")]
+    public IActionResult GetForecastByTime([FromQuery] DateTime startDate, [FromQuery] DateTime endDate) {
+        try {
+            if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
 
-            if (startDate >= endDate)
-            {
+            if (startDate >= endDate) {
                 return BadRequest("Start date must be before end date.");
             }
 
@@ -49,8 +42,7 @@ public class WeatherForecastController : BaseApiController
 
             var difference = (endDate - startDate).Days;
             if (_cache.TryGetValue(cacheKey, out IEnumerable<WeatherForecast>? forecasts)) return Ok(forecasts);
-            forecasts = Enumerable.Range(1, difference).Select(index => new WeatherForecast
-            {
+            forecasts = Enumerable.Range(1, difference).Select(index => new WeatherForecast {
                 Date = DateOnly.FromDateTime(startDate.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
@@ -64,8 +56,7 @@ public class WeatherForecastController : BaseApiController
 
             return Ok(forecasts);
         }
-        catch (Exception ex)
-        {
+        catch (Exception ex) {
             _logger.LogError(ex, "Error while getting forecast");
 
             return StatusCode(500, "An error occurred while getting the forecast. Please try again later.");
@@ -73,8 +64,7 @@ public class WeatherForecastController : BaseApiController
     }
 }
 
-public class WeatherForecast
-{
+public class WeatherForecast {
     public DateOnly Date { get; set; }
 
     public int TemperatureC { get; set; }
