d422ad0513a9bb421339993d5c7392c2ad692053
Deen <htcuser.wkp@gmail.com>
2023-03-01 13:22:44 +0530

    OrderItemAddValidator

--- Sample.Business/Validations/OrderValidators/OrderPlaceValidator.cs
+++ Sample.Business/Validations/OrderValidators/OrderPlaceValidator.cs
@@ -6,7 +6,12 @@ namespace Sample.Business.Validations.OrderValidators;
 public class OrderPlaceValidator : AbstractValidator<OrderAddDto> {
     public OrderPlaceValidator() {
 
-        RuleFor(dto => dto.CustomerId)
+        RuleFor(x => x.CustomerId)
             .SetValidator(new IdValidator("Customer ID"));
+
+        RuleFor(x => x.OrderItems)
+            .NotNull()
+            .Must(x => x.Any()).WithMessage("OrderItems should not be empty")
+            .ForEach(item => item.SetValidator(new OrderItemAddValidator()));
     }
 }
